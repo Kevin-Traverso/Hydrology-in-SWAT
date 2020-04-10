839f224311d23713bfa76abd2f6d4d687e1edf38
unknown <Kevin@P10MD009.utec.local>
2020-04-09 22:00:36 -0500

    Flow transformation function

---
+++ R/Flows2SwatCup.R
@@ -0,0 +1,9 @@
+#' @title Time series of flows to SWATCUP
+#' 
+#' @description
+#' 
+#' Bring a time series of daily / monthly / annual flows
+#' to the SWATCUP entry format
+#' 
+#' #' @author Kevin Arnold Traverso,
+#' \email {arnold.traverso@gmail.com}
