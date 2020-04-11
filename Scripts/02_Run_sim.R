e29acf2d66303cb9bb594453c36b2eca93e80fe8
unknown <Kevin@P10MD009.utec.local>
2020-04-11 18:48:14 -0500

    commenting code

--- Scripts/02_Run_sim.R
+++ Scripts/02_Run_sim.R
@@ -1,7 +1,17 @@
+# .........................................................................
+#                      HYDROLOGICAL SIMULATION IN SWAT
+# .........................................................................
 
-# HYDROLOGICAL SIMULATION IN SWAT
+'Hydrological simulation process using computational languaje R
+applied to the Mantaro river basin in Perú.
+
+Code developed for the Mg.Sc. thesis in Water Resources
+
+autor: Kevin Arnold Traverso Yucra
+mail: arnold.traverso@gmail.com'
 
 # Install/load required packages
+
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(SWATplusR)) devtools::install_github("chrisschuerz/SWATplusR")
 if(!require(zoo)) install.packages("zoo")
@@ -23,6 +33,7 @@ R_path <- "C:/Swat_model"
 
 # Outputs variables
 # the verables entered are the basic ones of the hydrological balance
+
 varb <- list(Pcp = define_output(file = "sub", variable = "PRECIP", unit = Sub_b),
              Flow = define_output(file = "rch", variable = "FLOW_OUT", unit = Sub_b),
              Wyld = define_output(file = "sub", variable = "WYLD", unit = Sub_b),
@@ -31,7 +42,6 @@ varb <- list(Pcp = define_output(file = "sub", variable = "PRECIP", unit = Sub_b
              Pet = define_output(file = "sub", variable = "PET", unit = Sub_b))
 
 # Running simulation -------------------------------------------------------
-
 Basic_sim <- run_swat2012(project_path = Dir_tx,
                           output = varb,
                           start_date = Start_d,
@@ -42,7 +52,7 @@ Basic_sim <- run_swat2012(project_path = Dir_tx,
                           add_date = TRUE, 
                           keep_folder = FALSE)
 
-# Mostrando resultados -----------------------------------------------------
+# Presenting results -------------------------------------------------------
 Sub_res <- Basic_sim %>% dplyr::select(-date) %>%
   summarise_all(., .funs = mean) %>% 
   gather(.) %>% 
@@ -58,7 +68,7 @@ Shp_riv <- shapefile(x = "./Shapefiles/DEM_UHM90mutmnet.shp")
 # merge simulated information to shapefiles
 Shp_Sb <- merge(Shp_Sb, Sub_res, by = "Subbasin")
 
-# displaying
+# Displaying ---------------------------------------------------------------
 pcp <- mapview(Shp_Sb, zcol = "Pcp", col.regions = viridisLite::viridis)
 wyld <- mapview(Shp_Sb, zcol = "Wyld", col.regions = viridisLite::inferno)
 pt <- mapview(Shp_Sb, zcol = "Et", col.regions = viridisLite::cividis)
